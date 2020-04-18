bbbdba53bdbf5083e561d6987fd2ba7aa7039b1d
Daniel Duris <dusoft@gmail.com>
2020-04-18 20:02:35 +0200

    added social network sharing tags

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<!doctype html>
+<?php
+$url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <!-- Google Tag Manager -->
@@ -17,6 +20,14 @@
     <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.1/build/pure-min.css" integrity="sha384-oAOxQR6DkCoMliIh8yFnu25d7Eq/PHS21PClpwjOTeU2jRSq11vu66rf90/cZr47" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.1/build/grids-responsive-min.css" crossorigin="anonymous">
     <link href="arrogance.css" rel="stylesheet">
+    <meta property="og:title" content="The Arrogance of Space Mapping Tool #ArroganceOfSpace">
+    <meta property="og:description" content="Useful tool to show how much space is taken in our cities by cars. Load an image and easily mark type of space for any city.">
+    <meta property="og:image" content="<?php echo $url; ?>favicon.png">
+    <meta property="og:url" content="<?php echo $url; ?>">
+    <meta name="twitter:title" content="The Arrogance of Space Mapping Tool #ArroganceOfSpace">
+    <meta name="twitter:description" content="Useful tool to show how much space is taken in our cities by cars. Load an image and easily mark type of space for any city.">
+    <meta name="twitter:image" content="<?php echo $url; ?>favicon.png">
+    <meta name="twitter:card" content="summary_large_image">
 </head>
 <body>
     <!-- Google Tag Manager (noscript) -->
